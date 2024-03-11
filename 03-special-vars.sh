5abf70b273fb01a9c52d92f264528963295d2666
Akhil <akhilmahendra035@gmail.com>
2024-03-11 16:08:05 +0530

    Special Variables

--- 03-special-vars.sh
+++ 03-special-vars.sh
@@ -1,5 +1,12 @@
-echo Value of 0 - $0
-echo Value of 1 - $1
-echo Value of 2 - $2
-echo "Value of * - $*"
-echo "Value of # - $# "
+echo Value of 0 - $0   # $0 prints the script name you're executing
+echo Value of 1 - $1   #$ arg1
+echo Value of 2 - $2   # $arg2
+echo "Value of * - $*"   # $* is going to print the used arguments/inputs
+echo "Value of # - $# "  # $# is going to print the number of arguments/inputs
+
+
+#  bash scriptName.sh  arg1  arg2  arg3
+
+# bash arg1  arg2  arg3 arg4  arg5  arg6 arg7  arg8  arg9  arg10
+#        1     2     3    4    5     6    7      8    9      10
+
