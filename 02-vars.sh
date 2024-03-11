9ccf4c19e33e9f4678812537c511c9e49e8bd29f
Akhil <akhilmahendra035@gmail.com>
2024-03-11 15:28:31 +0530

    Variables

---
+++ 02-vars.sh
@@ -0,0 +1,13 @@
+# variable with plain data
+
+a=10
+
+# variable with special character data(other than num,alphabets and underscore(_))
+# (In the data if we have special characters we go with double quotes)
+
+b="P@$s"
+
+#access variable --->  $variable_name / ${variable_name}
+
+echo $a
+echo $b
