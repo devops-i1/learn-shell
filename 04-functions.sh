ca4b9765232bc4b81f6e817f867296b262908a67
Akhil <akhilmahendra035@gmail.com>
2024-03-11 17:34:43 +0530

    functions example

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,9 +1,19 @@
-#IF we assign a name to set of commands, then that s "function".
+# If we assign a name to set of commands, then that s "function".
 
 # How to declare a function ?
-
 example() {
   echo "hello guys"
 }
 
 example   # calling a function
+
+
+# Functions have its own special variables
+
+example1() {
+  echo Value of 1 - $1
+  echo "Value of * - $*"
+  echo "Value of # - $#"
+}
+
+example1 10 20 30
