1c60f712e169313b3669f64220029c696bfed131
Akhil <akhilmahendra035@gmail.com>
2024-03-12 22:47:02 +0530

    for loop

--- 06-loops.sh
+++ 06-loops.sh
@@ -5,6 +5,7 @@
 # There are 2 major types of loops ;
 #     1) For loop       ( When you know something to be executed n number of time, we use for loop)
 #     2) While loop     ( When you don't know something to be executed n number of times, we use while loop)
+#  mostly used loop is for
 
 
 # while loop works withe expression
