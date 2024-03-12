e1459434dc60650a2c9a4b9f1a65554924d26788
Akhil <akhilmahendra035@gmail.com>
2024-03-12 15:46:46 +0530

    adding colors with example

--- 01-printing.sh
+++ 01-printing.sh
@@ -13,18 +13,18 @@ echo Hello World!
 #  Cyan       -36
 
 
-# Syntax  :    echo -e "/e[COLm MESSAGE WANT TO PRINT/e[0m"
+# Syntax  :    echo -e "\e[COLm MESSAGE WANT TO PRINT\e[0m"
 
 #           -e                  (enable color)
-#           "/e[COLm            (start color)      ----> COLm ---- color number
-#           /e[0m"              (disable color)
+#           "\e[COLm            (start color)      ----> COLm ---- color number
+#           \e[0m"              (disable color)
 
 
 # Example :
 
-echo -e "/e[31m red color/e[0m"
-echo -e "/e[32m green color/e[0m"
-echo -e "/e[33m yellow color/e[0m"
-echo -e "/e[34m blue color/e[0m"
-echo -e "/e[35m magenta color/e[0m"
-echo -e "/e[36m cyan color/e[0m"
+echo -e "\e[31m red color\e[0m"
+echo -e "\e[32m green color\e[0m"
+echo -e "\e[33m yellow color\e[0m"
+echo -e "\e[34m blue color\e[0m"
+echo -e "\e[35m magenta color\e[0m"
+echo -e "\e[36m cyan colo\e[0m"
