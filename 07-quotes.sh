8f46c2e18d8f585bf10d97fab76ad38fa303c69e
Akhil <akhilmahendra035@gmail.com>
2024-03-13 11:59:46 +0530

    quotes example

---
+++ 07-quotes.sh
@@ -0,0 +1,11 @@
+
+
+# Single Quotes will suppress the power of a special variable
+# Whenever you have any special variables, prefer to go with double quotes\
+
+
+# Example
+
+a=10
+echo "$a"     # ans 10
+echo '$a'     # ans $a
